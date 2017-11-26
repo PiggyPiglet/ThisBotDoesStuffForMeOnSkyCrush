c13a26979c6f125110c25787944c403b74c2c41e
PiggyPiglet <PiggyPiglet@WickedCubeMC.com>
2017-11-26 10:07:21 +0800

    Start
    phantom pls help me with this

---
+++ src/main/java/me/piggypiglet/skycrushauto/Main.java
@@ -0,0 +1,8 @@
+package me.piggypiglet.skycrushauto;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2017
+// https://www.piggypiglet.me
+// ------------------------------
+public class Main {
+}
